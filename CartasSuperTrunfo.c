136332b762acce8026f16041fbe16862e9aa2460
DaniDivino <danniel.valdivino.dv@gmail.com>
2025-08-30 00:00:37 +0000

    Criado cadastro das cartas de SuperTRUNFO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,73 @@
-#include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+#include <stdio.h> 
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+struct Carta { // Estrutura para armazenar as informações de uma carta
+    char estado; //variáveis criadas dentro da estrutura 
+    char codigo[4];
+    char nomeCidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+};
+struct Carta carta1, carta2; // Array que armazena 2 cartas
+
+// requisitando informações para a carta 1, e adicionando os valores nas variaveis criadas.
+printf ("Informações da Cidade CARTA 1 \n\n");
+
+printf ("Informe a letra que representa seu estado de A à H: ");
+scanf ("%c", &carta1.estado);
+printf ("Informe a letra do seu estado seguida de 2 numeros entre 01 e 04: ");
+scanf ("%s", &carta1.codigo);
+printf ("Informe o nome da sua cidade: ");
+scanf ("%s", &carta1.nomeCidade);
+printf ("Qual o número de habitantes na sua cidade: ");
+scanf ("%d", &carta1.populacao);
+printf ("Qual a área (em km²): ");
+scanf ("%f", &carta1.area);
+printf ("Qual o pib: ");
+scanf ("%f", &carta1.pib);
+printf ("Qual a quantidade de pontos turisticos da região: ");
+scanf ("%d", &carta1.pontosTuristicos);
+;
+// requisitando informações para a carta 2, e adicionando os valores nas variaveis criadas.
+printf ("\n\nInformações da Cidade CARTA 2 \n\n");
+printf ("Informe a letra que representa seu estado de A à H: ");
+scanf (" %c", &carta2.estado); 
+printf ("Informe a letra do seu estado seguida de 2 numeros entre 01 e 04: ");
+scanf ("%s", &carta2.codigo);
+printf ("Informe o nome da sua cidade: ");
+scanf ("%s", &carta2.nomeCidade);
+printf ("Qual o número de habitantes na sua cidade: ");
+scanf ("%d", &carta2.populacao);
+printf ("Qual a área (em km²): ");
+scanf ("%f", &carta2.area);
+printf ("Qual o pib: ");
+scanf ("%f", &carta2.pib);
+printf ("Qual a quantidade de pontos turisticos da região: ");
+scanf ("%d", &carta2.pontosTuristicos);
+
+// Mostrando os valores da carta 1 e 2 conforme solicitado noe exercicio.
+printf("\n\n Carta 1 \n"
+       "Estado: %c \n"
+       "Código: %s \n"
+       "Nome da Cidade: %s \n" 
+       "População: %d \n" 
+       "Área: %.2f km² \n"  
+       "PIB: %.2f bilhões de reais \n" 
+       "Número de Pontos Turísticos: %d \n\n", carta1.estado, carta1.codigo,carta1.nomeCidade,carta1.populacao,
+                                              carta1.area,carta1.pib,carta1.pontosTuristicos);
+printf("Carta 2 \n" 
+       "Estado: %c \n"
+       "Código: %s \n" 
+       "Nome da Cidade: %s \n"
+       "População: %d \n"  
+       "Área: %.2f km² \n"  
+       "PIB: %.2f bilhões de reais \n" 
+       "Número de Pontos Turísticos: %d \n ", carta2.estado,carta2.codigo,carta2.nomeCidade,carta2.populacao,
+                                              carta2.area,carta2.pib,carta2.pontosTuristicos);
+return 0;
+
+
 }
