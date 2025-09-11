163aca57f6591545986d7442a8bc38770e126119
DaniDivino <danniel.valdivino.dv@gmail.com>
2025-09-11 22:26:57 +0000

    Desafio Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,8 @@ struct Carta { // Estrutura para armazenar as informações de uma carta
     float area;
     float pib;
     int pontosTuristicos;
+    float densidade; // adicionado novas variaveis <= aventuteiro
+    float perCapita; // adicionado novas variaveis <= aventuteiro
 };
 struct Carta carta1, carta2; // Array que armazena 2 cartas
 
@@ -30,7 +32,10 @@ printf ("Qual o pib: ");
 scanf (" %f", &carta1.pib);
 printf ("Qual a quantidade de pontos turisticos da região: ");
 scanf (" %d", &carta1.pontosTuristicos);
-;
+
+carta1.perCapita = (float) carta1.pib / carta1.populacao; // calculo para atribuição de valores <= aventuteiro
+carta1.densidade = (float) carta1.populacao / carta1.area; // calculo para atribuição de valores <= aventuteiro
+
 // requisitando informações para a carta 2, e adicionando os valores nas variaveis criadas.
 printf ("\n\nInformações da Cidade CARTA 2 \n\n");
 printf ("Informe a letra que representa seu estado de A à H: ");
@@ -48,16 +53,21 @@ scanf (" %f", &carta2.pib);
 printf ("Qual a quantidade de pontos turisticos da região: ");
 scanf (" %d", &carta2.pontosTuristicos);
 
+carta2.perCapita = (float) carta2.pib / carta2.populacao; // calculo para atribuição de valores <= aventuteiro
+carta2.densidade = (float) carta2.populacao/ carta2.area; // calculo para atribuição de valores <= aventuteiro
+
 // Mostrando os valores da carta 1 e 2 conforme solicitado noe exercicio.
-printf("\n\n Carta 1 \n"
+printf("\n\nCarta 1 \n"
        "Estado: %c \n"
        "Código: %s \n"
        "Nome da Cidade: %s \n" 
        "População: %d \n" 
        "Área: %.2f km² \n"  
        "PIB: %.2f bilhões de reais \n" 
-       "Número de Pontos Turísticos: %d \n\n", carta1.estado, carta1.codigo,carta1.nomeCidade,carta1.populacao,
-                                              carta1.area,carta1.pib,carta1.pontosTuristicos);
+       "Número de Pontos Turísticos: %d \n" 
+       "PIB per Capita: %.2f \n"  // Impressão dos novos valores <= aventuteiro
+       "Densidade Populacional: %.2f \n\n", carta1.estado, carta1.codigo,carta1.nomeCidade,carta1.populacao, // Impressão dos novos valores <= aventuteiro
+                                              carta1.area,carta1.pib,carta1.pontosTuristicos, carta1.perCapita,carta1.densidade);
 printf("Carta 2 \n" 
        "Estado: %c \n"
        "Código: %s \n" 
@@ -65,9 +75,10 @@ printf("Carta 2 \n"
        "População: %d \n"  
        "Área: %.2f km² \n"  
        "PIB: %.2f bilhões de reais \n" 
-       "Número de Pontos Turísticos: %d \n ", carta2.estado,carta2.codigo,carta2.nomeCidade,carta2.populacao,
-                                              carta2.area,carta2.pib,carta2.pontosTuristicos);
+       "Número de Pontos Turísticos: %d \n"
+       "PIB per Capita: %.2f \n" // Impressão dos novos valores <= aventuteiro
+       "Densidade Populacional: %.2f \n ", carta2.estado,carta2.codigo,carta2.nomeCidade,carta2.populacao, // Impressão dos novos valores <= aventuteiro
+                                              carta2.area,carta2.pib,carta2.pontosTuristicos, carta2.perCapita,carta2.densidade);
 return 0;
-
 
 }
